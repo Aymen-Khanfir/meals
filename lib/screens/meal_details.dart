e5270f1b6dd28df6a375329c46e747c25211b33c
Aymen-Khanfir <aymenkhanfir56@gmail.com>
2024-06-23 14:04:02 +0100

    Initializing the project, getting started by outputing all the category that i've set in a dummy data, creating 2 models that hold the category and the meal definitions, create a category_grid_item that as as a grid container to display the categories, a meal screen where it displays the related meals for each of the categories and handle a navigation between the categories screen and the meal screen and for each meal I've added a meal_item where i used the Inkwell widget to add a new functionality that provide the user the posibility to tab on a meal, using a new package wich is the 'transparent_image' in order to add a new behavior and finaly add a new meal_detail screen where it hold the details for each meal and added to the navigator when the user tab over a specific meal that exists on a specific category

---
+++ lib/screens/meal_details.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealDetailsScreen extends StatelessWidget {
+  const MealDetailsScreen({
+    super.key,
+    required this.meal,
+  });
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(meal.title)),
+      body: FadeInImage(
+        image: NetworkImage(meal.imageUrl),
+        placeholder: MemoryImage(kTransparentImage),
+        height: 300,
+        width: double.infinity,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
